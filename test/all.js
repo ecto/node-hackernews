00a469f0f7aaac24ae42f13c04587cdeb750b616
Cam Pedersen <diffference@gmail.com>
2011-11-27 18:43:11 -0600

    [test] added hacky tests

---
+++ test/all.js
@@ -0,0 +1,41 @@
+var vows = require('vows'),
+    assert = require('assert'),
+    hn = require('../');
+
+var print = function (err, data) {
+  console.log('\n\n\n');
+  if (err) throw new Error (err);
+  data.items.forEach(function (item) {
+    console.log(item.title);
+  });
+}
+
+var verbose = function (err, data) {
+  console.log('\n\n\n');
+  if (err) throw new Error (err);
+  for (var i in data) {
+    console.log(i + ': ' + data[i]);
+  }
+}
+
+hn.page('front', print);
+hn.page('new', print);
+hn.profile('pg', verbose);
+hn.by('pg', verbose);
+
+/*
+vows.describe('Hacker News API client').addBatch({
+  'Pages': {
+    topic: hn,
+    'the front page': {
+      topic: function (hn) {
+        return hn.page('front');
+      },
+      'Front page can be retreived': function (front) {
+        //assert.isTrue (strawberry.isTasty());
+        console.log(front);
+      }
+    }
+  },
+}).export(module);
+*/
